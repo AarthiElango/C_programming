73584a91fd92d701322896cbaf6a047389b45bf6
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-04-26 12:11:40 +0530

    Create ascii.c

---
+++ Basic_concepts/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+//qn 10 
+    //char ch='A';
+    //printf("ASCII value of 'A' %d\n",ch);
+// qn 11   
+    char ch;
+    printf("enter a character:");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d \n",ch,ch);
+         }
