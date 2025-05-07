51b2bb0aa3f601eb4633a35c43056f9b2c912aa4
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-05-07 13:03:46 +0530

    Update size.c

--- Basic_concepts/size.c
+++ Basic_concepts/size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 void main(){
+  int x = 11;
+  int y;
+ // value of x doesn't change
+  y = sizeof(x++);// sizeof works in compile time ,x++ doesn't work 
+  printf("y= %d \n x=%d",y,x);
   printf("size of int %lu \n",sizeof(int));
   printf("size of char %lu \n",sizeof(char));
   printf("size of float %lu \n",sizeof(float));
