b750c86cfc36778d8fcbdfb1f387a22cb9148450
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-05-08 09:55:24 +0530

    Update itoc_ptr.c

--- pointers/itoc_ptr.c
+++ pointers/itoc_ptr.c
@@ -7,7 +7,7 @@ int main()
    a = 512;     //internally in binary=> 10 00000000 
    x[0] = 1;    //access byte0 =>10 00000001
    x[1] = 2;    //access byte1 =>10 same value
-   printf("%d\n",a);   //10 00000001
+   printf("%d\n",a);   //10 00000001 =>513 (machine dependent)
    return 0; 
 }
 
