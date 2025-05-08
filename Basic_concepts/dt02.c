36d8d17bafdb5bde7c06c9b830f3206567cae3e2
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-05-08 11:47:29 +0530

    Add files via upload

---
+++ Basic_concepts/dt02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char a = 012;
+    int n=012;
+    printf("%d",n);
+    printf("%d", a);
+
+    return 0;
+}
+
---
+++ Basic_concepts/dt03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    void *vptr,v;
+    
+    v = 0;
+    vptr = &v;
+    printf("%v", *vptr);
+    getchar();
+    return 0;
+}
+
