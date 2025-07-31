9b6dcd37ba7339b4f306db7cc949585cb6eee52b
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-07-31 18:42:47 +0530

    Create min_max.c
    min_max

---
+++ Day10/min_max.c
@@ -0,0 +1,12 @@
+//without comparision operator
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a = 10, b = 15;
+    int max = ((a + b) + (abs(a - b))) / 2;
+    int min = ((a + b) - (abs(a - b))) / 2;
+    printf("min %d max %d\n", min, max);
+  
+    return 0;
+}
